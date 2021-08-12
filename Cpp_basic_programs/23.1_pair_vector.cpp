fce72bcb96b826512432ca1b9b1de9c9d51d3386
Abhaya Nigam <abhaya.nigam13@gmail.com>
2021-08-12 11:24:30 +0530

    C++ programs

--- Cpp_basic_programs/23.1_pair_vector.cpp
+++ Cpp_basic_programs/23.1_pair_vector.cpp
@@ -2,46 +2,37 @@
 
 // Pair allows us to store pair element in the vector.
 
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-bool myCompare(pair <int,int > p1, pair <int, int> p2){
-    return (p1.first<p2.first);
-}
-
 int main()
 {
-    // //Initializing pair vector 
-    // pair <int,char> p;
-    // p.first = 3;
-    // p.second = 'f';
+    // Declaring vector of pair.
+    vector<pair <int,int> > v;
 
-//----------------------------------------------------------------
+    // Initialising 1st and 2nd element of the pair with array values
+    int arr[] = {10,20,30,40};
+    int arr1[] = {1,2,3,4,};
 
-    int arr[] = {10,16,7,14,5,3,2,9};
-    vector < pair<int,int > > v;
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(int i = 0; i<(sizeof(arr)/sizeof(arr[0])); i++){
-        // Now making a pair of every element with its index.
+    //Entering the values in vector pair
+    for (int i=0; i<n; i++)
+        v.push_back( make_pair(arr[i],arr1[i]) );
 
-        // // 1st method :
-        // pair<int, int> p;
-        // p.first = arr[i];
-        // p.second = i;
-
-        //2nd method : (The direct method).
-        v.push_back(make_pair(arr[i],i));
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
     }
+    
 
-    sort(v.begin(),v.end(),myCompare);
-    for(int i=0;i<v.size();i++){
-        arr[v[i].second] = i;
+    cout<< "After sorting an array :"<<endl;
+    sort(v.begin(),v.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        cout <<v[i].first << " "<< v[i].second<< endl; 
     }
-
-    for(int i=0;i<v.size();i++){
-        cout << arr[i]<< " ";
-    }
-
-    cout << endl;
 }
