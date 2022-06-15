5ea984bf4bad43a98db7d468131a976908e7b3e1
Abhaya Nigam <61977981+abhayanigam@users.noreply.github.com>
2022-06-15 11:53:43 +0530

    C++ programs

---
+++ Cpp_basic_programs/27_shortTrick/Print Hello World Without Semicolon/01_Using_if.cpp
@@ -0,0 +1,11 @@
+// Printing hello world without semicolon using if
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    if (printf("Hello World")){}
+    
+    return 0;
+}
---
+++ Cpp_basic_programs/27_shortTrick/Print Hello World Without Semicolon/03_Using_while_loop.cpp
@@ -0,0 +1,11 @@
+// Printing hello world without semicolon using while loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (! printf("Hello World")){}
+    
+    return 0;
+}
